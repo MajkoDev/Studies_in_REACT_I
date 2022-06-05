7d63f68a9dde20a304e1a232ede0dd94b6d83b77
Marian <hellomariandev@gmail.com>
2022-06-05 20:17:32 +0200

    Components and Props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+// Styling
 import './App.css';
+
+// Components
+import Hello from "./components/Hello";
+import Welcome from "./components/Welcome";
+import Greet from "./components/Greet";
 
 function App() {
   return (
     <div className="App">
+      {/* Components and Props  */}
+      <Hello title='function' />
+      <Welcome 
+          title='class' 
+          info='A little more informations' />
+        <Greet>This can also be greeting.</Greet> 
     </div>
   );
 }
