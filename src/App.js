1b320347ee9fcf5991352eeccd70d433458394c9
Marian <hellomariandev@gmail.com>
2022-06-07 20:55:55 +0200

    refs

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 // Styling
-import './App.css';
+import "./App.css";
 
 // Components
 import LifecycleA from "./components/LifecycleA";
 import Clock from "./components/LifecycleB";
 import FragmentDemo from "./components/FragmentDemo";
 import PureComp from "./components/PureComp";
+
 import RefsDemo from "./components/RefsDemo";
+import FocusInput from "./components/FocusInput";
+import RFParentInput from "./components/RFParentInput";
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       {/* <LifecycleA /> */}
       {/* <Clock /> */}
       {/* <FragmentDemo /> */}
       {/* <PureComp /> */}
-      <RefsDemo />
 
+      {/* <RefsDemo /> */}
+      {/* <FocusInput /> */}
+      <RFParentInput />
     </div>
   );
 }
