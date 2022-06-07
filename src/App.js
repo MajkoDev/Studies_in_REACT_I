078a3d9860ab3f7f2f0336accda0fae65cb8f573
Marian <hellomariandev@gmail.com>
2022-06-07 17:12:52 +0200

    lifecycle methods

--- src/App.js
+++ src/App.js
@@ -2,19 +2,16 @@
 import './App.css';
 
 // Components
-import Hello from "./components/Hello";
-import Welcome from "./components/Welcome";
-import Greet from "./components/Greet";
+import LifecycleA from "./components/LifecycleA";
+import Clock from "./components/LifecycleB";
+import FragmentDemo from "./components/FragmentDemo";
+import PureComp from "./components/PureComp";
 
 function App() {
   return (
     <div className="App">
-      {/* Components and Props  */}
-      <Hello title='function' />
-      <Welcome 
-          title='class' 
-          info='A little more informations' />
-        <Greet>This can also be greeting.</Greet> 
+      <LifecycleA />
+      <Clock />
     </div>
   );
 }
