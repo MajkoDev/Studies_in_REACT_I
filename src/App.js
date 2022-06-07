46a2769b85fb340404f500d1fceb00e82e623e4e
Marian <hellomariandev@gmail.com>
2022-06-07 20:57:54 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
 // Styling
-import './App.css';
-
-// Components
-import Hello from "./components/Hello";
-import Welcome from "./components/Welcome";
-import Greet from "./components/Greet";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      {/* Components and Props  */}
-      <Hello title='function' />
-      <Welcome 
-          title='class' 
-          info='A little more informations' />
-        <Greet>This can also be greeting.</Greet> 
-    </div>
-  );
+  return <div className='App'></div>;
 }
 
 export default App;
