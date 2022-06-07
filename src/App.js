0e88e4edbd18d2da3e264ed3c1031e659c75ed61
Marian <hellomariandev@gmail.com>
2022-06-07 21:23:43 +0200

    higher order components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 // Styling
 import "./App.css";
 
+// Components
+import ClickCounter from "./components/ClickCounter";
+import HoverCounter from "./components/HoverCounter";
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <ClickCounter name='Eric' />
+      <HoverCounter name='Adam' />
+    </div>
+  );
 }
 
 export default App;
