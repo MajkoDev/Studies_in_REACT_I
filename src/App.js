6ff282751a80affa0c5ac787fc0a185e23cfd898
Marian <hellomariandev@gmail.com>
2022-06-07 21:28:03 +0200

    render props

--- src/App.js
+++ src/App.js
@@ -5,11 +5,31 @@ import "./App.css";
 import ClickCounter from "./components/ClickCounter";
 import HoverCounter from "./components/HoverCounter";
 
+import ClickCounterTwo from "./components/ClickCounterTwo";
+import HoverCounterTwo from "./components/HoverCounterTwo";
+import User from "./components/User";
+import Counter from "./components/Counter";
+
 function App() {
   return (
     <div className='App'>
-      <ClickCounter name='Eric' />
-      <HoverCounter name='Adam' />
+      {/* <ClickCounter name='Eric' />
+      <HoverCounter name='Adam' /> */}
+
+      {/* <ClickCounterTwo />
+      <HoverCounterTwo />
+      <User render={(isLoggedIn) => (isLoggedIn ? "Adam" : "Guest")} /> */}
+
+      <Counter
+        render={(count, incrementCount) => (
+          <ClickCounterTwo count={count} incrementCount={incrementCount} />
+        )}
+      />
+      <Counter
+        render={(count, incrementCount) => (
+          <HoverCounterTwo count={count} incrementCount={incrementCount} />
+        )}
+      />
     </div>
   );
 }
