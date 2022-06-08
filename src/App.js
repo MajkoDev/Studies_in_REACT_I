660bef9ddcf8bc8445c3cfe0cfedf277fd9e4e24
Marian <hellomariandev@gmail.com>
2022-06-08 18:49:59 +0200

    useContext hook

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import React from "react";
+
 // Styling
 import "./App.css";
 
+// Components
+import ComponentA from "./components/ComponentA";
+import ComponentC from "./components/ComponentC";
+
+// creating Context
+export const UserContext = React.createContext();
+export const ChannelContext = React.createContext();
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      {/* providing Context */}
+      <UserContext.Provider value={"MajkoDev"}>
+        <ChannelContext.Provider value={"Slovakia"}>
+          <ComponentA />
+          <ComponentC />
+        </ChannelContext.Provider>
+      </UserContext.Provider>
+    </div>
+  );
 }
 
 export default App;
