95e897bbb32981073a47b97f3324545aa09e899f
Marian <hellomariandev@gmail.com>
2022-06-11 16:58:24 +0200

    useCallback hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 // Styling
 import "./App.css";
+
+// Components: fetch data with useReducer - useCallback - 
 import DataFetchingOne from "./components/DataFetchingOne";
 import DataFetchingTwo from "./components/DataFetchingTwo";
+import CallbackComponent from "./components/CallbackComponent";
 
 function App() {
   return (
     <div className='App'>
       {/* <DataFetchingOne /> */}
-      <DataFetchingTwo />
+      {/* <DataFetchingTwo /> */}
+      <CallbackComponent />
+
     </div>
   );
 }
